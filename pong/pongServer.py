a885040c0bf413deaef402015075b119756d3127
Dasikio <130800676+Dasikio@users.noreply.github.com>
2023-10-27 14:10:35 -0400

    ()

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -35,7 +35,7 @@ currPlayer = 0
 while True:
     playerSocket, playerAddress = server.accept()
     playerThread = threading.Thread(target=player_handle, args=(playerSocket,currPlayer,))
-    playerThread.start
+    playerThread.start()
     currPlayer +=1
 
 #server.listen(5)
